7a2cccdd9c1090715397063a24c74f042da1d65f
jamiesmithdesign <jamiesmithdesign@gmail.com>
2016-10-18 20:31:55 +0100

    added About div

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
-console.log('it works');
-
 $(document).ready(function() {
  
-  $("#owl-example").owlCarousel();
+  $("#owl-demo").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+
+      
+ 
+  });
+
+
  
 });
